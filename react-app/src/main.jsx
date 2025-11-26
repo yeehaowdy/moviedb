0efac9f99d095a30532543b2540a4409e654ebbd
tarjanyi.levi <tarjanyi.levi@gmail.com>
2025-11-26 12:50:05 +0100

    adasd

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,8 +1,11 @@
+/** TODO npm install @mui/material @emotion/react @emotion/styled react react-dom @mui/icons-material react-router react-router-dom */
 // TODO - make .env with VITE_TMDB_API_KEY=... and import to Netlify
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+
+const apiKey = import.meta.env.VITE_TMDB_API_KEY
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
