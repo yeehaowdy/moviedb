c4e7a31c2404e56808cbe862e0bf1589808d2bb4
tarjanyi.levi <tarjanyi.levi@gmail.com>
2025-12-03 12:58:40 +0100

    agakjsgia

--- react-app/tmdb-metadata.cjs
+++ react-app/tmdb-metadata.cjs
@@ -1,7 +1,9 @@
 module.exports.handler = async () => {
   try {
     const apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWJlMTg3OGI4MDg3OTUwMDgxNThkYzFiNzYxMThmYiIsIm5iZiI6MTc2NDA2Njg0OC41NjQ5OTk4LCJzdWIiOiI2OTI1ODYyMGVjN2IyMTAwNmVkOWMzYWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.wbWS-dgSjlECkEiaPmhbnrChoP8xEs_9GCAf4rMa8Eo";
+    console.log(apiKey)
     return { apiKey };
+
   } catch (error) {
     return { apiKey: null, error: error.message };
   }
