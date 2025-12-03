24d41e5b3096d7c5f7361abaf61fa7aa4b3da592
tarjanyi.levi <tarjanyi.levi@gmail.com>
2025-12-03 13:13:43 +0100

    asdf

--- react-app/src/components/ContentPagination.jsx
+++ react-app/src/components/ContentPagination.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import { logAction } from '../Backend/logAction';
+import { logAction } from '../Backend/logAction.js';
 
 export const ContentPagination = ({ page, setPage }) => {
   const handleChange = (event, value) => {
